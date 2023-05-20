fb3f94dd2ef165831c82846c27adf87499d360ef
Mason Kim <qwerty3345@naver.com>
2023-05-21 01:25:01 +0900

    [Chore] AddPhotoCell을 AddClothesCell로 네이밍 변경
    - AddPhotoView와 충돌되기에, 명시적이도록 함

---
+++ EasyCloset/EasyCloset/Sources/View/AddPhotoView.swift
@@ -0,0 +1,8 @@
+//
+//  AddPhotoView.swift
+//  EasyCloset
+//
+//  Created by Mason Kim on 2023/05/21.
+//
+
+import Foundation
--- EasyCloset/EasyCloset/Sources/View/ClothesCarouselCell.swift
+++ EasyCloset/EasyCloset/Sources/View/ClothesCarouselCell.swift
@@ -114,7 +114,7 @@ extension ClothesCarouselCell {
   
   private func setupCollectionView() {
     collectionView.registerCell(cellClass: ClothesCell.self)
-    collectionView.registerCell(cellClass: AddPhotoCell.self)
+    collectionView.registerCell(cellClass: AddClothesCell.self)
     collectionView.delegate = self
   }
   
@@ -122,7 +122,7 @@ extension ClothesCarouselCell {
     DataSource(collectionView: collectionView) { collectionView, indexPath, item in
       switch item {
       case .addClothes:
-        return collectionView.dequeueReusableCell(cellClass: AddPhotoCell.self, for: indexPath)
+        return collectionView.dequeueReusableCell(cellClass: AddClothesCell.self, for: indexPath)
       case .clothes(let clothes):
         let cell = collectionView.dequeueReusableCell(cellClass: ClothesCell.self, for: indexPath)
         cell.configure(with: clothes)
