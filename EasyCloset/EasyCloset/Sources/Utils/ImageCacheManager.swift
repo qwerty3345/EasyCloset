7625cafe0401509122236f7144035b833cec2ea5
Mason Kim <qwerty3345@naver.com>
2023-05-23 16:50:25 +0900

    [Refactor] 이미지 메모리 캐싱 개선
    - countLimit, totalCostLimit 을 통해서 
    이미지의 NSCache 메모리 캐싱 시 저장되는 갯수와 저장되는 용량에 제한을 줌

--- EasyCloset/EasyCloset/Sources/Utils/ImageCacheManager.swift
+++ EasyCloset/EasyCloset/Sources/Utils/ImageCacheManager.swift
@@ -9,22 +9,36 @@ import UIKit
 
 final class ImageCacheManager {
   
-  // MARK: - Properties
-  static let shared = ImageCacheManager()
-  private let cacheManager =  NSCache<NSString, UIImage>()
+  // MARK: - Configuration
   
-  // MARK: - LifeCycle
-  
-  private init() {}
-  
-  // MARK: - Public
-  
-  func get(for key: String) -> UIImage? {
-    cacheManager.object(forKey: key as NSString)
+  private enum Configuration {
+    static let countLimit = 100 // 총 100개 까지만 FIFO로 캐싱함
+    static let megaByteLimit = 200 * megaByteUnit // 메모리 캐싱에 200메가바이트 만큼 제약을 줌
+    static let megaByteUnit = 1024 * 1024
   }
   
-  func store(_ value: UIImage, for key: String) {
-    cacheManager.setObject(value, forKey: key as NSString)
+  // MARK: - Singleton
+  
+  static let shared = ImageCacheManager()
+  
+  private init() {
+    cache.countLimit = Configuration.countLimit
+    cache.totalCostLimit = Configuration.megaByteLimit
+  }
+  
+  // MARK: - Properties
+  
+  private let cache = NSCache<NSString, UIImage>()
+  
+  // MARK: - Public Methods
+  
+  func get(for id: UUID) -> UIImage? {
+    cache.object(forKey: id.uuidString as NSString)
+  }
+  
+  func store(_ value: UIImage, for id: UUID) {
+    let bytesOfImage = value.jpegData(compressionQuality: 1.0)?.count ?? 0
+    cache.setObject(value, forKey: id.uuidString as NSString, cost: bytesOfImage)
   }
   
 }
