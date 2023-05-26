9a14f0fc9a967ceef0f957bedfa599b27e5f93ee
Mason Kim <qwerty3345@naver.com>
2023-05-26 11:11:37 +0900

    [Chore] 불필요 Scheme 삭제, mock데이터 setup 비어있을 때만 하게 변경

--- EasyCloset/EasyCloset/Sources/Data/Repository/ClothesRepository.swift
+++ EasyCloset/EasyCloset/Sources/Data/Repository/ClothesRepository.swift
@@ -151,6 +151,8 @@ final class ClothesRepository: ClothesRepositoryProtocol {
   // 초기 데이터가 없을 때 테스트 용도로 Mock 데이터를 저장 해 주는 메서드
   #if DEBUG
   private func setupMockData() {
+    guard realmStorage.load(entityType: ClothesEntity.self).isEmpty else { return }
+    
     ClothesList.mock.clothesByCategory.forEach { (_, value: [Clothes]) in
       value.forEach { clothes in
         save(clothes: clothes)
