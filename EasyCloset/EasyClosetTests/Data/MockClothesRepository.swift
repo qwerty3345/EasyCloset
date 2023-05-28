f45ae258c2a829833234e6033deeee6b31abc72d
Mason Kim <qwerty3345@naver.com>
2023-05-28 21:49:45 +0900

    [Test] Mock Repository 구성
    - Mock 데이터를 내뱉게 구현함

---
+++ EasyCloset/EasyClosetTests/Data/MockClothesRepository.swift
@@ -0,0 +1,27 @@
+//
+//  MockClothesRepository.swift
+//  EasyClosetTests
+//
+//  Created by Mason Kim on 2023/05/28.
+//
+
+import Foundation
+import Combine
+
+final class MockClothesRepository: ClothesRepositoryProtocol {
+  func fetchClothesList() -> AnyPublisher<ClothesList, RepositoryError> {
+    return Just(ClothesList.mocks)
+      .setFailureType(to: RepositoryError.self)
+      .eraseToAnyPublisher()
+  }
+  
+  func save(clothes: Clothes) -> AnyPublisher<Void, RepositoryError> {
+    return Just(())
+      .setFailureType(to: RepositoryError.self)
+      .eraseToAnyPublisher()
+  }
+  
+  func removeAll() {
+    return
+  }
+}
---
+++ EasyCloset/EasyClosetTests/Data/MockStyleRepository.swift
@@ -0,0 +1,27 @@
+//
+//  MockStyleRepository.swift
+//  EasyClosetTests
+//
+//  Created by Mason Kim on 2023/05/28.
+//
+
+import Foundation
+import Combine
+
+final class MockStyleRepository: StyleRepositoryProtocol {
+  func fetchStyles() -> AnyPublisher<[Style], RepositoryError> {
+    return Just(Style.Mock.mocks)
+      .setFailureType(to: RepositoryError.self)
+      .eraseToAnyPublisher()
+  }
+  
+  func save(style: Style) -> AnyPublisher<Void, RepositoryError> {
+    return Just(())
+      .setFailureType(to: RepositoryError.self)
+      .eraseToAnyPublisher()
+  }
+  
+  func removeAll() {
+    return
+  }
+}
